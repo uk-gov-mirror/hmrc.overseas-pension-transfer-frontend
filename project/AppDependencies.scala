5668847e2d8c2b0512d36f16127043e831501151
grantrobson <grant.robson@digital.hmrc.gov.uk>
2026-09-03 07:24:01 +0100

    DLSN-612 Ran scalafmt

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   private val bootstrapVersion = "10.7.0"
   private val hmrcMongoVersion = "2.13.0"
-  private val PekkoVersion = "1.4.0"
+  private val PekkoVersion     = "1.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
@@ -13,15 +13,15 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "org.typelevel"                %% "cats-core"                  % "2.13.0",
     "com.googlecode.libphonenumber" % "libphonenumber"             % "9.0.35",
-    "io.github.samueleresca"       %% "pekko-quartz-scheduler"     % "1.3.0-pekko-1.1.x" withSources(),
-    
+    "io.github.samueleresca"       %% "pekko-quartz-scheduler"     % "1.3.0-pekko-1.1.x" withSources (),
+
     // Explicit pekko dependencies to ensure version alignment
-    "org.apache.pekko"             %% "pekko-actor"                % PekkoVersion,
-    "org.apache.pekko"             %% "pekko-actor-typed"          % PekkoVersion,
-    "org.apache.pekko"             %% "pekko-stream"               % PekkoVersion,
-    "org.apache.pekko"             %% "pekko-serialization-jackson" % PekkoVersion,
-    "org.apache.pekko"             %% "pekko-slf4j"                % PekkoVersion,
-    "org.apache.pekko"             %% "pekko-protobuf-v3"          % PekkoVersion
+    "org.apache.pekko" %% "pekko-actor"                 % PekkoVersion,
+    "org.apache.pekko" %% "pekko-actor-typed"           % PekkoVersion,
+    "org.apache.pekko" %% "pekko-stream"                % PekkoVersion,
+    "org.apache.pekko" %% "pekko-serialization-jackson" % PekkoVersion,
+    "org.apache.pekko" %% "pekko-slf4j"                 % PekkoVersion,
+    "org.apache.pekko" %% "pekko-protobuf-v3"           % PekkoVersion
   )
 
   val test: Seq[ModuleID] = Seq(
